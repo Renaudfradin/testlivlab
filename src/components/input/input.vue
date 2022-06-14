c6ee3ab5576301809a292f876b6a7de2faaa5619
Renaud <renaudfradin@gmail.com>
2022-06-14 17:24:42 +0200

    fixe

--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -16,6 +16,9 @@ export default defineComponent({
             let searchInput = this.inputSport.toLowerCase();
             let filterArray = this.sports.Sport.filter(item => item.name.toLowerCase().includes(searchInput))
             this.resultFound = filterArray;  
+            if (this.resultFound.length == 0) {
+                console.log("rien");
+            }
             console.log(this.resultFound);
         }
     },
@@ -34,6 +37,9 @@ export default defineComponent({
         <div v-for="(sport , id) in sports.Sport" v-bind:key="id">
             {{sport.name}}
         </div>
+    </div>
+    <div v-else-if="this.resultFound.length == 0">
+        <p>Désolé aucun resultat de recherche pour votre recherche. Vérifier l'orthographe ou effectuez un recherche differente</p>
     </div>
     <div v-for="(sports , id) in resultFound" v-bind:key="id">
         {{sports.name}}
