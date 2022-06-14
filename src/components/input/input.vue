8d4b60a997f51e40ed4b52e4d4a9d4271013019d
Renaud Fradin <renaudfradin@gmail.com>
2022-06-14 15:02:42 +0200

    first commit

---
+++ src/components/input/input.vue
@@ -0,0 +1,45 @@
+<script>
+import { defineComponent } from 'vue'
+import jsonSport from '../Data/data.json'
+
+export default defineComponent({
+    data(){
+        return{
+            sports: jsonSport,
+            inputSport: null,
+            resultFound: null
+        }
+    },
+    methods:{
+        resultJsonSport(){
+            console.log(this.resultFound);
+            let searchInput = this.inputSport.toLowerCase();
+            let filterArray = this.sports.Sport.filter(item => item.name.toLowerCase().includes(searchInput))
+            this.resultFound = filterArray;  
+            console.log(this.resultFound);
+        }
+    },
+    beforeUpdate(){
+        this.resultJsonSport();
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h1>{{inputSport}}</h1>
+        <input type="text" v-model="inputSport">
+    </div>
+    <div v-if="resultFound == null">
+        <div v-for="(sport , id) in sports.Sport" v-bind:key="id">
+            {{sport.name}}
+        </div>
+    </div>
+    <div v-for="(sports , id) in resultFound" v-bind:key="id">
+        {{sports.name}}
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<script>
+import { defineComponent } from "vue";
+import Input from "../components/input/input.vue"
+
+export default defineComponent({
+  components: { Input },
+  setup() {
+    
+  },
+})
+</script>
+
+<template>
+  <div>
+    <h1>Sport</h1>
+    <Input />
+  </div>
+</template>
+
+<style>
+
+</style>
